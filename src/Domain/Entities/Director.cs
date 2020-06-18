9985e1ce2acd40d14f4c14b6404f260f8c1cdab1
Konrad Dybkowski <kdybkowski1@gmail.com>
2020-06-18 12:08:11 +0200

    Added Entities

---
+++ src/Domain/Entities/Director.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Director
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTimeOffset DateOfBirth { get; set; }
+
+        [Required]
+        public string PlaceOfBirth { get; set; }
+
+        public DateTimeOffset? DateOfDeath { get; set; }
+
+        public ICollection<Movie> Movies { get; set; }
+            = new List<Movie>();
+    }
+}
---
+++ src/Domain/Entities/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Movie
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(1500)]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTimeOffset ReleaseDate { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Genres { get; set; }
+
+        [ForeignKey("DirectorId")]
+        public Director Director { get; set; }
+
+        public Guid DirectorId { get; set; }
+    }
+}
