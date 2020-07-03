dc55ad9d3a444216a8fc11779137c06655912e86
Konrad Dybkowski <kdybkowski1@gmail.com>
2020-07-03 20:35:47 +0200

    Validating Data and Reporting Validation Errors

---
+++ src/Application/Common/ValidationAttributes/MovieTitleMustBeDifferentFromDescriptionAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Application.Movies;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common
+{
+    public class MovieTitleMustBeDifferentFromDescriptionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var movie = (MovieForCreationDto)validationContext.ObjectInstance;
+
+            if (movie.Title == movie.Description)
+            {
+                return new ValidationResult(ErrorMessage,
+                    new[] { nameof(MovieForCreationDto) });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- src/Application/Movies/MovieForCreationDto.cs
+++ src/Application/Movies/MovieForCreationDto.cs
@@ -1,16 +1,37 @@
-﻿using System;
+﻿using Application.Common;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Application.Movies
 {
-    public class MovieForCreationDto
+    [MovieTitleMustBeDifferentFromDescription
+    (ErrorMessage = "Title must be different from description")]
+    public class MovieForCreationDto /*: IValidatableObject*/
     {
+        // If property validation fails, class level validation will not occur even if it is custom attribute.
+
+        // Custom attributes are executed  before Validate method gets called.
+
+        [Required(ErrorMessage = "You should fill out a title.")]
+        [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
         public string Title { get; set; }
+        [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
         public string Description { get; set; }
         public string ReleaseDate { get; set; }
         public string Genres { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult(
+        //            "The provided description should be different from the title.",
+        //            new[] { "MovieForCreationDto" });
+        //    }
+        //}
     }
 }
